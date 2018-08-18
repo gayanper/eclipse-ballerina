87696324e24564256246ae78d8d8b76b8e869420
Gayan Perera <gayanper@gmail.com>
2018-08-18 11:25:03 +0530

    initial project commit

---
+++ org.gap.eclipse.ballerina.test/src/main/java/org/gap/eclipse/ballerina/core/ActivatorTest.java
@@ -0,0 +1,19 @@
+package org.gap.eclipse.ballerina.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+* Sample integration test. In Eclipse, right-click > Run As > JUnit-Plugin. <br/>
+* In Maven CLI, run "mvn integration-test".
+*/
+public class ActivatorTest {
+
+	@Test
+	public void veryStupidTest() {
+		assertEquals("org.gap.eclipse.ballerina.core",Activator.PLUGIN_ID);
+		assertTrue("Plugin should be started", Activator.getDefault().started);
+	}
+}
